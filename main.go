4287fd4b8f0678ea3673340f85fd73f44bb8ca62
kysnm <tokyoincidents.g@gmail.com>
2017-08-25 11:29:24 +0900

    Add log examples

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
-
 import (
-	"github.com/labstack/echo"
+	"log"
 	"net/http"
-)
+	"os"
 
+	"github.com/comail/colog"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"time"
+)
 
 func GetTest() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		c.Echo().Logger.Debug("debug at handler")
 		return c.JSON(http.StatusOK, "hello world!")
 	}
 }
 
 func main() {
 	e := echo.New()
+
+	file, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(file)
+
+	e.Debug = true
+	e.Logger.SetOutput(file)
+
+	log.Println("foo")
+
+	log.Println("debug: from colog")
+
+	var out = colog.NewCoLog(file, "", 0)
+	out.SetFormatter(&colog.JSONFormatter{
+		TimeFormat: time.RFC3339,
+		Flag:       log.Lshortfile,
+	})
+
+	log.SetOutput(out)
+
+	log.Println("debug: colog in json format")
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Output: file,
+	}))
 
 	e.GET("/test", GetTest())
 
